007b538a77681ba3464e11215f543c6527527d12
radiant-ai <bodia1206@gmail.com>
2023-03-05 14:00:35 -0600

    fixes

--- src/main/java/fun/milkyway/milkyschemupload/SchematicsCommand.java
+++ src/main/java/fun/milkyway/milkyschemupload/SchematicsCommand.java
@@ -5,17 +5,10 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Subcommand;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.bukkit.command.CommandSender;
-
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -42,39 +35,20 @@ public class SchematicsCommand extends BaseCommand {
                 return;
             }
             commandSender.sendMessage("§eUploading schematic... Please wait...");
-            var post = new HttpPost("https://file.io/");
-            var apiKey = MilkySchemUpload.getInstance().getConfig().getString("apiKey", "");
-            if (!apiKey.isEmpty()) {
-                post.addHeader("Authorization", "Bearer " + apiKey);
-            }
-            var fileBody = new FileBody(file, ContentType.DEFAULT_BINARY);
-            var builder = MultipartEntityBuilder.create();
-            builder.addPart("file", fileBody);
-            var entity = builder.build();
-
-            post.setEntity(entity);
             try {
-                var client = HttpClientBuilder.create()
-                        .setDefaultRequestConfig(RequestConfig.custom()
-                                .setConnectTimeout(15000)
-                                .setSocketTimeout(15000)
-                                .setConnectionRequestTimeout(15000)
-                                .build())
+                var httpRequest = HttpRequest.newBuilder()
+                        .uri(java.net.URI.create("https://temp.sh/"+file.getName()))
+                        .header("Content-Type", "multipart/form-data")
+                        .timeout(java.time.Duration.ofSeconds(20))
+                        .PUT(HttpRequest.BodyPublishers.ofFile(file.toPath()))
                         .build();
-
-                var response = client.execute(post);
-
-                var responseString = new BasicResponseHandler().handleResponse(response);
-
-                if (responseString == null) {
-                    commandSender.sendMessage("§cError while uploading schematic: "+response.getStatusLine().getStatusCode());
+                var response = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() != 200) {
+                    commandSender.sendMessage("§cError while uploading schematic: §f" + response.body());
                     return;
                 }
-
-                var json = new Gson().fromJson(responseString, JsonObject.class);
-                var link = json.get("link").getAsString();
-                commandSender.sendMessage("§aSchematic uploaded: §f" + link);
-                MilkySchemUpload.getInstance().getLogger().info("§aSchematic uploaded: §f" + link);
+                commandSender.sendMessage("§aSchematic uploaded: §f" + response.body());
+                MilkySchemUpload.getInstance().getLogger().info("§aSchematic uploaded: §f" + response.body());
             } catch (Exception e) {
                 commandSender.sendMessage("§cError while uploading schematic: §f" + e.getMessage());
                 e.printStackTrace();
